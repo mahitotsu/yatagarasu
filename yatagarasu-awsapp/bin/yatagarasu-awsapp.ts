2a17cfddd1266e60278c063c28dcf6b24cfbe60c
Akio Kanashiki <akio@mail.com>
2024-09-07 22:18:56 +0900

    modified

--- yatagarasu-awsapp/bin/yatagarasu-awsapp.ts
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-import { App } from 'aws-cdk-lib';
-import 'source-map-support/register';
-import { YtgStack } from '../lib/YtgStack';
-
-const app = new App();
-new YtgStack(app, 'YtgStack', {
-  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: 'ap-northeast-1', }
-})
--- yatagarasu-webapp/pages/index.vue
+++
@@ -1,4 +0,0 @@
-<template>
-    <h1>Hello World</h1>
-    <p>This is an index page.</p>
-</template>
