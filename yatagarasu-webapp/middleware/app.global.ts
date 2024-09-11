8b72ba60f0efb6d3a1bd3c3a2dceabae3ea24334
Akio Kanashiki <akio@mail.com>
2024-09-11 23:42:33 +0900

    modified

--- yatagarasu-webapp/middleware/app.global.ts
+++
@@ -1,5 +0,0 @@
-export default defineNuxtRouteMiddleware(async (to) => {
-    if (to.path == '/') {
-        return await navigateTo('/decisions/list-decisions');
-    }
-})
--- yatagarasu-webapp/pages/index.vue
+++ yatagarasu-webapp/pages/index.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
     <h1>Hello World</h1>
     <p>This is an index page.</p>
     <ul>
@@ -7,7 +6,4 @@
             <NuxtLink to="/decisions/list">Decisions</NuxtLink>
         </li>
     </ul>
-=======
-    <div>Redirect to default application page ...</div>
->>>>>>> eb4939f9319050838b647f32a2dc87cad2025f3f
 </template>
