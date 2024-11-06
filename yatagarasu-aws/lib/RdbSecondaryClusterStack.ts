6210220f7a17b9e7e901d564e9da45f62a6c8371
Akio Kanashiki <akio@mail.com>
2024-11-06 23:36:28 +0900

    modified

---
+++ yatagarasu-aws/lib/RdbSecondaryClusterStack.ts
@@ -0,0 +1,30 @@
+import { RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
+import { IVpc, SubnetSelection } from "aws-cdk-lib/aws-ec2";
+import { CfnDBCluster, ClusterInstance, DatabaseCluster, IClusterEngine } from "aws-cdk-lib/aws-rds";
+
+interface RdbSecondaryClusterStackProps extends StackProps {
+    vpc: IVpc,
+    vpcSubnets: SubnetSelection,
+    rdbClusterEngine: IClusterEngine,
+    globalClusterIdentifier: string,
+}
+
+export class RdbSecondaryClusterStack extends Stack {
+    constructor(scope: any, id: string, props: RdbSecondaryClusterStackProps) {
+        super(scope, id, props);
+
+        const dbCluster = new DatabaseCluster(this, 'RdbPrimaryCluster', {
+            engine: props.rdbClusterEngine,
+            vpc: props.vpc,
+            vpcSubnets: props.vpcSubnets,
+            writer: ClusterInstance.serverlessV2,
+            serverlessV2MinCapacity: 0.5,
+            deletionProtection: false,
+            removalPolicy: RemovalPolicy.DESTROY,
+        });
+        const cfnRdb = dbCluster.node.defaultChild as CfnDBCluster;
+        cfnRdb.masterUsername = undefined;
+        cfnRdb.masterUserPassword = undefined;
+        cfnRdb.globalClusterIdentifier = props.globalClusterIdentifier;
+    }
+}
